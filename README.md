4f69bad2cef4ca0f39993b83d7f669dab7ded7a7
Elliott Bolzan <elliottbolzan@gmail.com>
2021-07-08 18:34:41 -1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ariadne
+
+### GORILA
+
+GORILA is Louis **G**odart and Jean-Pierre **O**livier's ***R**ecueil des **i**nscriptions en **L**inéaire **A***, published from 1976 to 1985 in *Études Crétoises*. The text's five volumes constitute the most complete corpus of Linear A inscriptions.
+
+### Linear A
+
+[Linear A](https://en.wikipedia.org/wiki/Linear_A) is an undeciphered Cretan writing system believed to have been used from around 1800 BC to 1400 BC. The language encoded by the script is often referred to as Minoan. The script was succeeded by [Linear B](https://en.wikipedia.org/wiki/Linear_B), which was deciphered by Michael Ventris in the 1950s.
+
+### Problem
+
+Thanks to its authors, GORILA is available [online](https://cefael.efa.gr/result.php?site_id=1&serie_id=EtCret) for free. Unfortunately, the text is hard to navigate: the website only offers navigation between individual pages, which quickly becomes unwieldy given that the volumes are close to 2,000 pages long in total.
+
+### Solution
+
+This repository contains a script that can be used to scrape the images and combine them into 5 PDFs (one per volume). For ease of access, the repository also contains the 5 PDFs (see the "volumes" folder).
+
+To my knowledge, this is the only place online where GORILA is available as a PDF. Enjoy!
---
+++ ariadne.py
@@ -0,0 +1,54 @@
+from os import listdir, makedirs
+from os.path import dirname, isdir, isfile, join, realpath
+from PIL import Image
+import urllib.request
+
+gorila_path = dirname(realpath(__file__)) + "/"
+
+volume_count = 5
+volume_names = ["1_1976", "2_1979", "3_1976", "4_1982", "5_1985"]
+pages = [372, 162, 236, 222, 384]
+
+images_path = str(gorila_path) + "images/"
+volumes_path = str(gorila_path) + "volumes/"
+
+if not isdir(images_path):
+    makedirs(images_path)
+
+if not isdir(volumes_path):
+    makedirs(volumes_path)
+
+# Download.
+for volume_index in range(0, len(volume_names)):
+    volume_name = volume_names[volume_index]
+    volume_path = images_path + str(volume_index + 1) + "/"
+    if not isdir(volume_path):
+        makedirs(volume_path)
+    max_pages = pages[volume_index]
+    for page_number in range(1, max_pages + 1):
+        online_path = "https://cefael.efa.gr/apps/library/services/images/?f=EtCret_21-" + volume_name + "_" + \
+            f"{page_number:03d}" + ".jpg&p=jpg%2F150%2FEtCret%2FEtCret_21-" + \
+            volume_name + "%2FEtCret_21-" + volume_name + "_VolumeBroche"
+        image_path = volume_path + str(page_number) + ".jpg"
+        try:
+            urllib.request.urlretrieve(online_path, image_path)
+        except Exception as e:
+            print(e)
+            continue
+
+# Combine into PDFs.
+for volume_index in range(1, volume_count + 1):
+    images = []
+    images_for_volume_path = images_path + str(volume_index) + "/"
+    files = sorted([f for f in listdir(images_for_volume_path) if isfile(join(
+        images_for_volume_path, f)) and ".jpg" in f], key=lambda x: int(x.split(".")[0]))
+    for file in files:
+        filepath = images_for_volume_path + file
+        temp_image = Image.open(filepath)
+        keep_image = temp_image.copy()
+        images.append(keep_image)
+        temp_image.close()
+    volume_path = volumes_path + str(volume_index) + ".pdf"
+    first_image = images[0]
+    first_image.save(volume_path, "PDF", resolution=100.0,
+                     save_all=True, append_images=images)
